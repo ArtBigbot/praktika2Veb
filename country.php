7596acb0c129e288aa12a2976fbbb8091fdc92bb
ArtBigbot <kartartur@mail.ru>
2019-10-18 23:00:10 +0300

    first commit

---
+++ country.php
@@ -0,0 +1,20 @@
+<?php 
+class country { 
+   var code;
+   var name;
+   var continent;
+   var region;
+   var surfacearea;
+   var indepyear;
+   var population;
+   var lifeexpectancy;
+   var gnp;
+   var gnpold;
+   var localname;
+   var governmentform;
+   var headofstate;
+   var capital;
+   var code2;
+
+} 
+?>

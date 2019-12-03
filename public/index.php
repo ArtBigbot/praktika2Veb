6ec6f4480a27fc3d5f5821096e73c56110feb135
ArtBigbot <kartartur@mail.ru>
2019-12-03 20:59:57 +0200

    Make some changes for redbean library

--- public/index.php
+++ public/index.php
@@ -4,23 +4,27 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
   use Slim\Factory\AppFactory;
 
+
   //require_once __DIR__ . '/../bootstrap/app.php';
   require __DIR__ . '/../vendor/autoload.php';
-  //require_once( realpath( dirname( __FILE__ ) ).'/vendor/autoload.php' );
-  $container = new Container();
+  require __DIR__ . '/../vendor/rb.php';
+  R::setup('mysql:host=localhost;dbname=praktika2WS','root','');
 
-  AppFactory::setContainer($container); 
+  //require_once( realpath( dirname( __FILE__ ) ).'/vendor/autoload.php' );
+  //$container = new Container();
+
+  //AppFactory::setContainer($container); 
 
   $app = AppFactory::create(); 
 
-  $container = $app->getContainer();
-
+ // $container = $app->getContainer();
+/*
   $container->set('Country', function (ContainerInterface $c) {
       $view = $c->get('view'); // retrieve the 'view' from the container
       return new Country($view);
 
   });
-
+*/
 
 
 
@@ -84,10 +88,28 @@ $app->get('/hello/{name}', function (Request $request, Response $response, $args
 //$app-> group('/api',function ()  {
    // $app->group('/v1',function() use ($api){
         //get methods
-        $app->get('/all','\CountryAPI\Country:getCountries');// get all countries
+       // $app->get('/all','\CountryAPI\Country:getCountries');// get all countries
+        $app->get('/countryById/{id}', function($request, $response, $args){
+           // $api = $this->api;
+          
+           // $sql = new DbQuery();
+           // $sql->select('country.*');
+           /*
+            $sql->from('country','country');
+            $countries = Db::getInstance()->executeS($sql);
+            return $api->response([
+                'sucess'=>true,
+                'countries'=>$countries
+            ]);
+            */
+            $id =$args['id'];
+            $country= R::load('country',$id);
+            return $country;
+        });
+
   
         $app->get('/world/country/continent/:name',\Country::class .':getCountryByContinent');// get continent by name
-       // $app->get('/world/country/{name}/city/all','\app\db\country:getAllCities');
+        $app->get('/world/country/{name}/city/all','\app\db\country:getAllCities');
         
     
         //post methods

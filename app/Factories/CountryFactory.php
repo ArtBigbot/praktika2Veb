cce0f789a5c1e03b9c4438cdd3419ef3f27c59c6
ArtBigbot <kartartur@mail.ru>
2019-12-04 23:14:07 +0200

    Create countryFactory to return Country as new object

--- app/Factories/CountryFactory.php
+++ app/Factories/CountryFactory.php
@@ -3,8 +3,8 @@ namespace  App\Factories;
 class CountryFactory{
     function __invoke()
     {
-        $country = new praktika2Veb\app\Models\CountryModel();
-        return new \praktika2Veb\app\Contry($city);
+        $country = new \App\Models\CountryModel();
+        return new \praktika2Veb\app\Country($city);
     }
 }
 

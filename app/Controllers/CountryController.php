ec7c51997072c2b41aefb93174940f006beb79be
ArtBigbot <kartartur@mail.ru>
2019-12-11 21:46:07 +0200

    correct all methods

--- app/Controllers/CountryController.php
+++ app/Controllers/CountryController.php
@@ -1,20 +1,90 @@
-<?php
+<?php 
 namespace App\Controllers;
-class CountryController
-{
-    protected $container;
+use \RedBeanPHP\R as R;
+//use praktika2Veb\vendor\rb;
 
-    function __construct($container)
-    {
-        $this->container = $container;
+//require_once "vendor/autoload.php";
+//\Slim\Slim::registerAutoloader();
+//$app = new \Slim\Slim();
+//var_dump(app);
+require 'C:\xampp\htdocs\praktika2Veb\vendor\rb.php';
+R::setup( 'mysql:host=localhost;dbname=praktika2ws','root', 'root' );
+class CountryController  { 
+  public  $country;
+   protected $container;
+   
+  
+   public  function getCountries( ){
+     if(R::testConnection()){
+        $countries = R::findAll( 'country' );
+        echo '<pre>' ;print_r($countries);echo '</pre>';
+     }else{
+         exit('There is no connection to database');
+     }
+   }
+   public function getCountryByContinent($continentName){ 
+     /*  
+    $api = $this->api;
+    $country = new CountryObject((string) $continent);
+    if(!Validate::isLoadedObject($continentName)){
+        $api->response->setStatus(404);
+        return $api->response([
+            'success'=>false,
+            'message'=>'Continent was not found'
+           // 'success'=> true,
+        ]);
     }
-    function __invoke($request, $response, $args)
-    {
-        $country = $this->container->get('contryModel');//create uor country from CountryFactory in DIC
-        $args['countries'] = $country->getCountries();
-       return $this->container->get('render')->render($response,'show.phtml',$args);
+    return $api->response([
+        'success' => true,
+        'message'=>'Countries were found',
+        'country'=>[
+            'code'=>$country->code,
+            'name'=>$country->name,
+            'continent'=>$country->continent,
+            'region'=>$country->region,
+            'surfacearea'=>$country->surfacearea,
+            'indepyear'=>$country->indepyear,
+            'population'=>$country->population,
+            'lifeexpectancy'=>$country->lifeexpectancy,
+            'gnp'=>$country->gnp,
+            'gnpold'=>$country->gnpold,
+            'localname'=>$country->localname,
+            'governmentform'=>$country->governmentform,
+            'headofstate'=>$country->headofstate,
+            'capital'=>$country->capital,
+            'code2'=>$country->code2,
+        ],
+    ]);
+    */
+ if (R::testConnection()){
+     $countries = R::findLike('country',array(
+         'continent' => array($continentName)
+        ));
+        echo '<pre>' ;print_r($countries);echo '</pre>';
+ }
+
+   }
+     //else{
+        /*
+        return $api->response([
+            'success'=>true,
+            'message'=>'',
+           // 'success'=> true,
+           $sql->select(`country.`continent`');
+           $sql->from('country','country');
+           $countries = Db::getInstance()->executeS($sql);
        
-    }
+           return $api->response([
+               'sucess'=>true,
+               'countries'=>$countries
+           ]);
+          
+          }
+       
+        ]);*/
+  //  }
 
-}
+
+} 
+
 ?>
